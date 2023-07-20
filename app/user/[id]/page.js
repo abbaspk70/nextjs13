64e54b9be744507a1bb487ffd06a99c45b345ead
abbaspk70 <abbaspk70@hotmail.com>
2023-07-20 13:57:24 +0500

    user page change

--- app/user/[id]/page.js
+++ app/user/[id]/page.js
@@ -1,11 +1,19 @@
-
-export default async function UserData({user}) {
+import Image from "next/image";
+export default async function UserData({ user }) {
   return (
-    <div>hi
-      <h1>from user page : {user.id}</h1>
-      <h3>{user.first_name} {user.last_name}</h3>
-      <p>{user.email}</p>
-
+    <div className="text-center">
+      <h1 className="text-xl">User Information Page</h1>
+      <div className="bg-red-100 grid grid-cols-2 gap-4">
+      <div className="left bg-red-400">
+        <Image src={user.avatar} width='100' height='100' alt="User Image" />
+      </div>
+      <div className="right bg-red-800">
+        <h1>from user page : {user.id}</h1>
+        <h3>{user.first_name} {user.last_name}</h3>
+        <p>{user.email}</p>
+      </div>
     </div>
+    </div>
+
   );
 }
